4a327d03066f3ff064840ebdf84db18b9948a72f
KaitlynWickson <kwickson28@gmail.com>
2022-10-26 12:23:10 -0400

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = ()=>{
+    return(
+        <>
+            <h1>The Crispy Duchess</h1>
+            <h2>A place for bakers</h2>
+        </>
+    
+    )
+}
+
+export default Header
